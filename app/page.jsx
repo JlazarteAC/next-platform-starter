1efb1dd2bc9198b746fca939b816724da6c47a9a
JlazarteAC <125142346+JlazarteAC@users.noreply.github.com>
2024-09-01 20:30:51 +0800

    psuh

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image'; // Import the Image component
 import { Card } from 'components/card';
 import { RandomQuote } from 'components/random-quote';
 import { Markdown } from 'components/markdown';
 import { ContextAlert } from 'components/context-alert';
 import { getNetlifyContext } from 'utils';
+import Herobanner from 'public/images/IMG_1149.png';
 
 const cards = [
     //{ text: 'Hello', linkText: 'someLink', href: '/' }
@@ -20,7 +22,7 @@ The card content below is fetched by the client-side from \`/quotes/random\` (se
 
 const postDynamicContentExplainer = `
 On Netlify, Next.js Route Handlers are automatically deployed as [Serverless Functions](https://docs.netlify.com/functions/overview/).
-Alternatively, you can add Serverless Functions to any site regardless of framework, with acccess to the [full context data](https://docs.netlify.com/functions/api/).
+Alternatively, you can add Serverless Functions to any site regardless of framework, with access to the [full context data](https://docs.netlify.com/functions/api/).
 
 And as always with dynamic content, beware of layout shifts & flicker! (here, we aren't...)
 `;
@@ -30,6 +32,18 @@ const ctx = getNetlifyContext();
 export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
+            {/* Hero Banner Section */}
+            <section className="relative">
+                <Image
+                    src={Herobanner}
+                    alt="Hero Banner"
+                    layout="responsive"
+                    width={1920} 
+                    height={1080}
+                    className="object-cover"
+                />
+            </section>
+            
             <section className="flex flex-col items-start gap-3 sm:gap-4">
                 <ContextAlert />
                 <h1 className="mb-0">Netlify Platform Starter - Next.js</h1>
